f232b749d8bc61d8dd011ec9ae5cc5f9d2bbfa87
Alex <alejandro10montes@gmail.com>
2024-12-07 15:12:45 +0100

    Back to Top butto in ArticleList

--- app/src/main/java/com/alexmncn/flemingshop/ui/components/ArticleList.kt
+++ app/src/main/java/com/alexmncn/flemingshop/ui/components/ArticleList.kt
@@ -2,6 +2,7 @@ package com.alexmncn.flemingshop.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,13 +15,20 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowUpward
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -30,21 +38,33 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.view.NestedScrollingParentHelper
 import androidx.navigation.NavController
 import com.alexmncn.flemingshop.data.model.Article
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.math.BigInteger
 
 @Composable
 fun ArticleList(total: Int, articles: List<Article>, listName: String, onShowMore: () -> Unit, navController: NavController) {
     val screenHeight = LocalConfiguration.current.screenHeightDp.dp  // Altura de la pantalla
+    val scrollState = rememberScrollState() // Scroll principal
+    val gridState = rememberLazyGridState() // Scroll del grid de articulos
+    val coroutineScope = rememberCoroutineScope()
+    val gridColumns = 2 // Numero de columnas en el grid de articulos
 
     // Progreso de la barra que muestra la cantidad de artículos cargados
     val articlesLoadedProgress = articles.size.toFloat() / total.toFloat()
@@ -65,7 +85,7 @@ fun ArticleList(total: Int, articles: List<Article>, listName: String, onShowMor
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .verticalScroll(rememberScrollState())
+            .verticalScroll(scrollState)
     ) {
         // Header
         Column(
@@ -94,12 +114,13 @@ fun ArticleList(total: Int, articles: List<Article>, listName: String, onShowMor
         ) {
             Spacer(modifier = Modifier.height(10.dp))
 
-            // LazyVerticalGrid para mostrar los artículos en cuadricula
+            // Artículos en cuadricula
             LazyVerticalGrid(
-                columns = GridCells.Fixed(2),
+                columns = GridCells.Fixed(gridColumns),
+                state = gridState,
                 verticalArrangement = Arrangement.spacedBy(4.dp),
                 horizontalArrangement = Arrangement.spacedBy(10.dp),
-                modifier = Modifier.heightIn(max = screenHeight) // Limita el alto del grid para que el scroll no cause errores
+                modifier = Modifier.heightIn(max = screenHeight), // Limita el alto del grid para que el scroll no cause errores
             ) {
                 items(articles.size) { index ->
                     ArticleCard(
@@ -109,25 +130,62 @@ fun ArticleList(total: Int, articles: List<Article>, listName: String, onShowMor
                 }
             }
 
-            // Barra de progreso
-            Column(
+            // Barra progreso y boton subir
+            Box(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(vertical = 10.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(
-                    text = "${articles.size} de $total artículos",
-                    style = MaterialTheme.typography.bodyMedium
-                )
-                Spacer(modifier = Modifier.height(5.dp))
-                LinearProgressIndicator(
-                    progress = { articlesLoadedProgress },
+                // Contenedor para alineamiento
+                Column(
+                    horizontalAlignment = Alignment.End,
+                    verticalArrangement = Arrangement.Center,
                     modifier = Modifier
-                        .width(200.dp)
-                        .height(4.dp)
-                        .clip(RoundedCornerShape(10.dp))
-                )
+                        .fillMaxSize(),
+                ){
+                    // Boton para volver arriba
+                    IconButton(
+                        onClick = {
+                            coroutineScope.launch {
+                                gridState.animateScrollToItem(0)
+                                scrollState.animateScrollTo(0)
+                            }
+                        },
+                        modifier = Modifier
+                            .background(MaterialTheme.colorScheme.primary , CircleShape)
+                            .size(35.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowUpward,
+                            contentDescription = "Subir",
+                            tint = MaterialTheme.colorScheme.onPrimary
+                        )
+                    }
+                }
+
+                // Barra de progreso
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .align(Alignment.Center),
+                ) {
+                    Text(
+                        text = "${articles.size} de $total artículos",
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+
+                    Spacer(modifier = Modifier.height(5.dp))
+
+                    LinearProgressIndicator(
+                        progress = { articlesLoadedProgress },
+                        modifier = Modifier
+                            .width(200.dp)
+                            .height(4.dp)
+                            .clip(RoundedCornerShape(10.dp))
+                    )
+                }
             }
 
             // Botón "Mostrar más"
