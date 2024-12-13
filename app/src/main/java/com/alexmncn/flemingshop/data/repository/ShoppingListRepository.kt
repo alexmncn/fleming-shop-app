be84b505a427c07a130753aef66aae3591736cac
alexmncn <118453946+alexmncn@users.noreply.github.com>
2024-12-13 17:12:58 +0100

    Update ShoppingListRepository.kt

--- app/src/main/java/com/alexmncn/flemingshop/data/repository/ShoppingListRepository.kt
+++ app/src/main/java/com/alexmncn/flemingshop/data/repository/ShoppingListRepository.kt
@@ -9,4 +9,16 @@ class ShoppingListRepository(db: AppDatabase) {
     suspend fun getAllArticles(): List<ArticleItem> {
         return articleItemDao.getAll()
     }
+
+    suspend fun getArticleByCodebar(codebar: Int): ArticleItem? {
+        return articleItemDao.getByCodebar(codebar)
+    }
+
+    suspend fun deleteArticleByCodebar(codebar: Int) {
+        articleItemDao.deleteByCodebar(codebar)
+    }
+
+    suspend fun insertArticle(articleItem: ArticleItem) {
+        articleItemDao.insert(articleItem)
+    }
 }
